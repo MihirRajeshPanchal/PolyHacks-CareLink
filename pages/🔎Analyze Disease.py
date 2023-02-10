67806b7dcd441560b318c44a6f04ca02e018cdc1
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-02-10 12:30:22 +0530

    added 2 models

--- pages/🔎Analyze Disease.py
+++ pages/🔎Analyze Disease.py
@@ -3,12 +3,33 @@ from scripts import tts
 
 
 def mlmodels():
+    import pandas as pd
+    df=pd.read_csv("../datasets/heart.csv")
+    x = df.drop('target',axis='columns')
+    y = df['target']
+    from sklearn.model_selection import train_test_split
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state = 0)
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.fit_transform(x_test)
     if option=="Logistic Classification":
-        pass
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression()
+        model.fit(x_train, y_train)
+        return model.predict(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
     elif option=="Support Vector Machine Classifications":
         pass
     elif option=="Naive Bayes Classification":
         pass
+    elif option=="K Nearest Neighbor Classification":
+        pass
+    elif option=="Decision Tree Classifier":
+        from sklearn.tree import DecisionTreeClassifier
+        model = DecisionTreeClassifier()
+        model.fit(x_train, y_train)
+        return model.predict(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
+    
     
 
 st.title('Cardiac Prediction')
@@ -41,7 +62,7 @@ thal = st.text_input("Enter Thalassemia (0 - 2) : ",placeholder='1')
 
 option = st.selectbox(
     'Model Making',
-    ('Logistic Classification', 'Support Vector Machine Classifications', 'Naive Bayes Classification'))
+    ('Logistic Classification', 'Support Vector Machine Classifications', 'Naive Bayes Classification' , 'K Nearest Neighbor Classification',))
 
 if st.button('Login'):
     if age == '' or sex == '' or cp== '' or trestbps=='' or chol == '' or fbs == "" or restecg == "" or thalach=="" or exang=="" or oldpeak=="" or slope=="" or ca=="" or thal=="":
